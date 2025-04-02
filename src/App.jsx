6e519f435eeb2c0cd2b58041eeb70d85402f8765
goutham120203 <gouthampaladugu12@gmail.com>
2025-04-02 11:07:11 +0530

    com Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+import React,{useState} from "react";
 import './App.css'
 
 function App(){
+      let[todoInput,setTodoInput]=useState("Add task")
+      let [todoList,setTodoList]=useState([{
+        id:1,
+        task:'learn React'
+      },
+      {
+        id:2,
+        task:'learn Angular'
+      }     
+      ]);
+
+      let nextId = 3;
+
+      function addNewTodo(){
+        if(todoInput==""){
+          alert("Add some task");
+        }
+        else{
+          let newTodos =[
+            ...todoList,
+            {
+              id:nextId++,
+              task:todoInput
+            }
+          ]
+
+          setTodoList(newTodos)
+          setTodoInput("")
+        }
+      }
+
+      function deleteTodo(id){
+          let updatedTodos=todoList.filter(
+            (todo)=>{
+              return todo.id!=id
+            }
+          )
+          setTodoList(updatedTodos)
+
+      }
+
   return(
     <div className="container mt-5 w-50">
         <h3 className="text-center">Todo App Using React</h3>
         <div className="input-group">
-          <input className="form-control"  type="text" />
-          <button className="btn btn-primary">Add</button>
+          <input className="form-control" onChange={(e)=>{
+            let task=e.target.value;
+            setTodoInput(task);
+          }} type="text" value={todoInput} />
+          <button className="btn btn-primary" onClick={()=>{
+            addNewTodo()
+          }}>Add</button>
         </div>
-        
         <ul className="list-group mt-4">
-          <li className="list-group-item">
-            <p>Todo</p>
-            <button className="btn btn-primary">❌</button>
-          </li>
-          <li className="list-group-item">
-            <p>Todo</p>
-            <button className="btn btn-primary">❌</button>
-          </li>        
-          </ul>
+        {todoList.map((todo)=>{
+          return (
+            <li className="list-group-item">
+              <p>{todo.task}</p>
+              <button className="btn btn-primary" onClick={()=>{
+                deleteTodo(todo.id)
+              }}>❌</button>
+            </li>
+          )
+        })}
+        </ul>
     </div>
   )
 }
